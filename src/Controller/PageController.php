1bbd1de2a2fd8a67f71c4b765cee9a8ad626b439
Paul LOPEZ <p.lopez@alienor.net>
2025-11-24 21:37:01 +0100

    responsive fix

---
+++ src/Controller/PageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\StatisticsService;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PageController extends AbstractController
+{
+
+    #[Route('/le-collectif', name: 'app_collectif')]
+    public function le_collectif(): Response
+    {
+
+        return $this->render('page/collectif.html.twig', [
+        ]);
+    }
+
+    #[Route('/maison-des-livreurs', name: 'app_maison_des_livreurs')]
+    public function maisonDesLivreurs(): Response
+    {
+
+        return $this->render('page/maison-des-livreurs.html.twig', [
+        ]);
+    }
+}
