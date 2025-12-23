b01a6414a27c700fcdee66cfdca305002d48fa39
nbvaerh <nbvaerh@ssh02.cluster100.gra.hosting.ovh.net>
2025-12-23 12:49:22 +0000

    feat: migration contrainte unicité précédente

---
+++ migrations/Version20251223124812.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251223124812 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX unique_commitment ON commitment (candidate_list_id, proposition_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX unique_commitment ON commitment');
+    }
+}
